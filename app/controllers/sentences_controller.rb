742b041eb3ceb437a83bdf26bd23ac769d2085f5
adamscottbaxter <adamscottbaxter@gmail.com>
2017-08-08 21:29:49 -0500

    Add form

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -2,4 +2,19 @@ class SentencesController < ApplicationController
 	def index
 		@sentence = Sentence.new
 	end
+
+	def create
+		@sentence = Sentence.new(sentence_params)
+		if @sentence.save
+			redirect_to sentences_path
+		else
+			render 'index'
+		end
+	end
+
+	private
+
+	def sentence_params
+		params.require(:sentence).permit(:content)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
 	root to: 'sentences#index'
+	resources :sentences, only: [:index, :create]
 end
